bb83bfa33e97355de9555925a49728385dde8d06
kych4n <hades1@kakao.com>
2025-02-19 23:43:57 +0900

    Refactor [#5] 필요하지 않은 테스트 제거

--- src/test/java/apl/filtering/FilteringApplicationTests.java
+++ src/test/java/apl/filtering/FilteringApplicationTests.java
@@ -1,13 +1,11 @@
 package apl.filtering;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FilteringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
